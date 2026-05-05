d723658baa78c5e071ececa7c4549a300fee61d4
Juliano <72839366+julianocgc@users.noreply.github.com>
2026-05-05 18:00:12 -0300

    Envio de arquivos
    Envio de arquivos Vue.

---
+++ 2_projeto_vue/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+  <h1>Olá mundo!</h1>
+  <PrimeiroComponente />
+  <p>O dado de app é: {{ testando }}</p>
+  </div>
+</template>
+
+<script>
+
+import PrimeiroComponente from './components/PrimeiroComponente.vue'
+
+  export default{
+    name: 'App',
+    components: {
+      PrimeiroComponente,
+    },
+
+    data(){
+      return{
+        testando: "Teste"
+      }
+    }
+  }
+</script>
---
+++ 2_projeto_vue/src/components/LifeCycle.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>Meu nome é: {{ nome }}</h1>
+</template>
+
+<script>
+export default{
+    name: 'LifeCycle',
+    data(){
+        return {
+            nome: 'Ainda não sei'
+        }
+    }
+}
+
+</script>
