7ccab5331aad18929ac2e37db3e766381304e9f0
Juliano <72839366+julianocgc@users.noreply.github.com>
2026-05-19 12:07:34 -0300

    Envio de arquivos
    Envio de arquivos Vue.

--- 2_projeto_vue/src/App.vue
+++ 2_projeto_vue/src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <div>
-  <h1>Olá mundo!</h1>
   <PrimeiroComponente />
   <p>O dado de app é: {{ testando }}</p>
+  <LifeCycle />
+  <Pessoa />
   </div>
 </template>
 
 <script>
-
 import PrimeiroComponente from './components/PrimeiroComponente.vue'
+import LifeCycle from './components/LifeCycle.vue'
 
   export default{
     name: 'App',
     components: {
       PrimeiroComponente,
+      LifeCycle,
     },
 
     data(){
--- 2_projeto_vue/src/components/LifeCycle.vue
+++ 2_projeto_vue/src/components/LifeCycle.vue
@@ -9,6 +9,17 @@ export default{
         return {
             nome: 'Ainda não sei'
         }
+    },
+    created(){
+        setTimeout(() => {
+                this.nome = 'Vitor'
+        }, 1000);
+    
+    },
+    mounted(){
+        setTimeout(() => {
+                this.nome = 'Pedro'
+        }, 2000);
     }
 }
 
